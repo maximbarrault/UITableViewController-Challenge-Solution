c9f8ce25ff7f39bce04db894fbe336a52071885b
Oliver Feldman <oliverfeldman@Olivers-MacBook-Air.local>
2014-09-02 11:27:09 +0200

    extra credit

--- OJFTableViewController.m
+++ OJFTableViewController.m
@@ -72,14 +72,14 @@
     
     // Configure the cell...
     if (indexPath.section == 0){
-        cell.textLabel.text = @"I am in section 0";
+        cell.textLabel.text = @"I am in section 0"; cell.backgroundColor = [UIColor redColor];
     }
     else if (indexPath.section == 1){
-        cell.textLabel.text = @"another section";
+        cell.textLabel.text = @"another section"; cell.backgroundColor = [UIColor blueColor];
     }
     
     else {
-        cell.textLabel.text = [NSString stringWithFormat:@"cell %i", indexPath.row];
+        cell.textLabel.text = [NSString stringWithFormat:@"cell %i", indexPath.row]; cell.backgroundColor = [UIColor yellowColor];
     }
     
     return cell;

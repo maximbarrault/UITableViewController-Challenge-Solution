ebac20934cfea7d29ae5bc33e746afd300918e8e
Oliver Feldman <oliverfeldman@Olivers-MacBook-Air.local>
2014-09-02 11:20:41 +0200

    solved the challenge

---
+++ OJFTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OJFTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Oliver Feldman on 02/09/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OJFTableViewController : UITableViewController
+
+@end
